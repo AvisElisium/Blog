c952241625a068eb20435e4e64b3f259c1d7a3c0
AvisElisium <adrianjeziorski00@gmail.com>
2023-03-19 03:26:39 +0100

    added user auth state representation in navbar and added menu for logged in users

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,9 +1,15 @@
 ﻿import {AppBar, Box, Container, Divider, Grid, Link, Toolbar, Typography} from "@mui/material";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import {Link as RouterLink} from "react-router-dom";
+import {useContext} from "react";
+import {AuthContext} from "../../context/AuthContext";
+import NavbarUserMenu from "./NavbarUserMenu";
 
 
 const Navbar = () => {
+    
+    const {currentUser} = useContext(AuthContext);
+    
     return (
         <AppBar position={"static"}>
             <Container maxWidth={"xl"}>
@@ -12,17 +18,26 @@ const Navbar = () => {
                     <Grid item xs={8} md={4}>
                         <Typography noWrap fontWeight={700} variant={"h2"} variantMapping={{
                             h2: "h1",
-                        }} sx={{userSelect: "none"}} display={"flex"} justifyContent={"center"} >
+                        }} sx={{userSelect: "none"}} display={"flex"} justifyContent={{
+                            xs: "flex-start",
+                            md: "center",
+                        }} >
                             Blog
                         </Typography>
                     </Grid>
                     <Grid item xs={4} display={"flex"} justifyContent={"flex-end"} alignItems={"center"}>
-                        <Link component={RouterLink} to={"/login"} color={"inherit"} display={"flex"} underline={"hover"} padding={0.25}>
-                            <AccountCircleIcon />
-                            <Typography marginLeft={0.5}>
-                                Login
-                            </Typography>
-                        </Link>
+                        {currentUser === null &&
+                            <Link component={RouterLink} to={"/login"} color={"inherit"} display={"flex"} underline={"hover"} padding={0.25}>
+                                <AccountCircleIcon />
+                                <Typography marginLeft={0.5}>
+                                    Login
+                                </Typography>
+                            </Link>
+                        }
+
+                        {currentUser && 
+                            <NavbarUserMenu />
+                        }
                     </Grid>
                 </Grid>
             </Container>
---
+++ client/src/components/navbar/NavbarUserMenu.tsx
@@ -0,0 +1,94 @@
+﻿import {
+    Box,
+    ClickAwayListener,
+    Container,
+    Grid,
+    Grow,
+    Menu,
+    MenuItem,
+    MenuList,
+    Paper,
+    Popper,
+    Typography
+} from "@mui/material";
+import {useContext, useState, MouseEvent} from "react";
+import {AuthContext} from "../../context/AuthContext";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import {useNavigate} from "react-router-dom";
+
+
+const NavbarUserMenu = () => {
+    
+    const {currentUser, logout} = useContext(AuthContext);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const navigate = useNavigate();
+    
+    const open = Boolean(anchorEl);
+    
+    const handleClick = (event: MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    
+    return (
+        <Grid container display={"flex"} justifyContent={"flex-end"} alignItems={"center"}>
+            <Box 
+                onClick={(e) => handleClick(e)}
+                display={"flex"}
+                justifyContent={"flex-end"}
+                alignItems={"center"}
+                sx={{
+                    ":hover": {
+                        cursor: "pointer"
+                    }
+                }}
+            >
+                <AccountCircleIcon />
+                <Typography padding={1}>
+                    {currentUser?.username}
+                </Typography>
+            </Box>
+            <Popper
+                open={open}
+                anchorEl={anchorEl}
+                placement={"bottom-start"}
+                transition
+                disablePortal
+            >
+                {({TransitionProps, placement}) => (
+                    <Grow
+                        {...TransitionProps}
+                        style={{
+                            transformOrigin:
+                                placement === 'bottom-start' ? 'left top' : 'left bottom',
+                        }}
+                    >
+                        <Paper>
+                            <ClickAwayListener onClickAway={handleClose}>
+                                <MenuList
+                                    onKeyDown={() => {}}
+                                    autoFocusItem={open}
+                                >
+                                    <MenuItem onClick={() => {
+                                        logout();
+                                        handleClose();
+                                    }}
+                                    >
+                                        Logout
+                                    </MenuItem>
+                                </MenuList>
+                            </ClickAwayListener>
+                        </Paper>
+                    </Grow>
+                )}
+                
+            </Popper>
+            
+        </Grid>
+    )
+}
+
+export default NavbarUserMenu;
